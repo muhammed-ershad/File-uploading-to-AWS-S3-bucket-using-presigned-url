e8d67eb03379e530935fe63f1117495092ef6d1e
Muhammed Ershad P <ershad.p@enfintechnologies.com>
2024-06-10 11:23:46 +0530

    .env variable name changed, code cleanup

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
-import { generatePresignedUrlForFileUpload, generatePresignedUrlForFileDownload } from "./url.generating.functions.js";
-
-
+import {
+  generatePresignedUrlForFileUpload,
+} from "./url.generating.functions.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));        
+app.use(express.urlencoded({ extended: true }));
 
-app.post('/put-presigned-url', async (req, res) => {
-    const { fileName } = req.body
-    const response = await generatePresignedUrlForFileUpload(fileName);
-    if(response) res.json({ success: true, data: response });
-    else res.json({ success: false });
-})
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.post('/get-presigned-url', async (req, res) => {
-    const { fileName } =  req.body
-    const presignedUrl = await generatePresignedUrlForFileDownload(fileName);
-    if(presignedUrl) res.json({ success: true, data: presignedUrl });
-    else res.json({ success: false });
-})
+// Serve the index.html file from the root directory
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
+app.post("/put-presigned-url", async (req, res) => {
+  const { fileName } = req.body;
+  const response = await generatePresignedUrlForFileUpload(fileName);
+  if (response) res.json({ success: true, data: response });
+  else res.json({ success: false });
+});
 
 app.listen(process.env.PORT || 4000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 4000}`);
+  console.log(`Server is running on port ${process.env.PORT || 4000}`);
 });
--- url.generating.functions.js
+++ url.generating.functions.js
@@ -1,50 +1,39 @@
-import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import {
+  S3Client,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const REGION = process.env.AWS_REGION;
-const BUCKET_NAME = process.env.AWS_BUCKET_NAME;
+const REGION = process.env.IMAGE_UPLOAD_AWS_REGION;
+const BUCKET_NAME = process.env.IMAGE_UPLOAD_AWS_BUCKET_NAME;
 
 const credentials = {
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.IMAGE_UPLOAD_AWS_ACCESS_KEY,
+  secretAccessKey: process.env.IMAGE_UPLOAD_AWS_SECRET_KEY,
 };
 
 const s3Client = new S3Client({ region: REGION, credentials });
 
-
+//Put object url and file name
 export const generatePresignedUrlForFileUpload = async (fileName) => {
   try {
-    const newFileName = (Date.now()).toString() + fileName
+    const newFileName = Date.now().toString() + fileName;
     const params = {
-        Bucket: BUCKET_NAME,
-        Key: `/uploads/${newFileName}`,
-        ContentType: "image/*",
+      Bucket: BUCKET_NAME,
+      Key: `uploads/${newFileName}`,
+      ContentType: "image/*",
     };
 
     const command = new PutObjectCommand(params);
     const url = await getSignedUrl(s3Client, command, {
       expiresIn: 300,
-    }); 
-    console.log(newFileName);
-    console.log(url)
-    return { url, newFileName };
-  } catch (err) {
-    console.log("Error", err);
-  }
-};
-
-export const generatePresignedUrlForFileDownload = async (fileName) => {
-  try {
-    const command = new GetObjectCommand({
-      Bucket: BUCKET_NAME,
-      Key: `/uploads/${fileName}`,
     });
-    const url = await getSignedUrl(s3Client, command, { expiresIn: 300 });
-    console.log(url);
-    return url;
+    // console.log(newFileName);
+    // console.log(url)
+    return { url, newFileName };
   } catch (err) {
     console.log("Error", err);
   }
